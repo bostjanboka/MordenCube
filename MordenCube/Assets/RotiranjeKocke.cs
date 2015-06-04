16815454d734e6e0cc3aba5c9cdae9936aee0b65
bostjanboka <bohtebostjan@gmail.com>
2015-06-04 18:52:23 +0200

    komit

---
+++ MordenCube/Assets/RotiranjeKocke.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotiranjeKocke : MonoBehaviour {
+
+	// Use this for initialization
+	Vector2 rotacija;
+
+	Vector2 oldPos;
+	void Start () {
+		oldPos = Vector2.zero;
+		rotacija = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Touch touch in Input.touches) {
+			if(touch.phase.Equals(TouchPhase.Began) || touch.phase.Equals(TouchPhase.Ended)){
+				oldPos = touch.position;
+
+			}
+			else if(touch.phase.Equals(TouchPhase.Moved)){
+				rotacija = oldPos - touch.position;
+				oldPos = touch.position;
+			}
+		}
+
+		gameObject.transform.Rotate (rotacija);
+		rotacija = Vector2.zero;
+	}
+}
